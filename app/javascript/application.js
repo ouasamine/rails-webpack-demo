458b536ca06c97f896948852dace4958f55af9dd
Amine <ouas.amine@outlook.com>
2023-02-21 15:37:05 +0100

    Implement React

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (<h1>Hello React World!</h1>);
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
